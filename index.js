e9aa859f4fd3d084a609bb783c577861eb39b9c5
AYUSTARK <ayushrj65@gmail.com>
2021-08-12 16:34:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const fs = require('fs');
 const axios = require('axios');
 const cors = require('cors');
 const config = require('./config.json');
-const { Client } = require('whatsapp-web.js');
+const {Client} = require('whatsapp-web.js');
 const SESSION_FILE_PATH = './session.json';
-
-app.use(cors())
+const app = express();
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
-
-process.title = "whatsapp-node-api";
+app.use(cors())
+process.title = "whatsapp-send-api";
 global.client = new Client({
     puppeteer: {
         headless: true,
@@ -21,22 +20,22 @@ global.client = new Client({
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--unhandled-rejections=strict'
-    ]},
+        ]
+    },
     session: sessionCfg
 });
 
 global.authed = false;
 
-const app = express();
-
 const port = process.env.PORT || config.port;
 //Set Request Size Limit 50 MB
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 client.on('qr', qr => {
+    console.log('QR', "QR file created")
     fs.writeFileSync('./components/last.qr', qr);
 });
 
@@ -51,10 +50,10 @@ client.on('authenticated', (session) => {
         }
         authed = true;
     });
-
     try {
         fs.unlinkSync('./components/last.qr')
-    } catch(err) {}
+    } catch (err) {
+    }
 });
 
 client.on('auth_failure', () => {
@@ -69,7 +68,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (config.webhook.enabled) {
-        axios.post(config.webhook.path, { msg })
+        axios.post(config.webhook.path, {msg})
     }
 })
 client.initialize().then(response => {
@@ -83,13 +82,13 @@ const chatRoute = require('./components/chatting');
 const authRoute = require('./components/auth');
 // const contactRoute = require('./components/contact');
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     console.log(req.method + ' : ' + req.path);
     next();
 });
-app.use('/chat',chatRoute);
+app.use('/chat', chatRoute);
 // app.use('/group',groupRoute);
-app.use('/auth',authRoute);
+app.use('/auth', authRoute);
 // app.use('/contact',contactRoute);
 
 app.listen(port, () => {
