25a33d6c83c0845cd61550170936e84c2a8539fa
AYUSTARK <ayushrj65@gmail.com>
2021-08-20 15:08:18 +0530

    Update chatting.js

--- components/chatting.js
+++ components/chatting.js
@@ -4,7 +4,7 @@ router.post('/sendmessage/:phone', async (req,res) => {
     let phone = req.params.phone;
     let message = req.body.message;
     if (phone === undefined || message === undefined) {
-        res.send({ status:"error", message:"please enter valid phone and message" })
+        res.status(400).json({ status:"error", message:"please enter valid phone and message" })
     } else {
         const sanitized_number = phone.toString().replace(/[- )(]/g, ""); // remove unnecessary chars from the number
         const final_number = `91${sanitized_number.substring(sanitized_number.length - 10)}`; // add 91 before the number here 91 is country code of India
@@ -14,11 +14,11 @@ router.post('/sendmessage/:phone', async (req,res) => {
                 client.sendMessage(resp._serialized, message).then((response) => {
                     console.log("Send Message: ", response)
                     if (response.id.fromMe) {
-                        res.send({status: 'success', message: `Message successfully sent to ${final_number}`, response, resp})
+                        res.status(200).json({status: 'success', message: `Message successfully sent to ${final_number}`, response, resp})
                     }
                 }).catch(err => {
                     console.error(err)
-                    res.send({status: 'err', "message": err.body, "err": err.message})
+                    res.status(400).json({status: 'err', "message": err.body, "err": err.message})
                 });
             } else {
                 console.log(final_number, "Mobile number is not registered"+ resp);
